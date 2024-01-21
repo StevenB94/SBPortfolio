dabef30340907366359311aa68134038e816a6e5
StevenB94 <89290120+StevenB94@users.noreply.github.com>
2024-01-20 22:34:14 -0800

    started about me section

---
+++ src/about.js
@@ -0,0 +1,9 @@
+import './about.css'; 
+
+function About() {
+    return (
+       <h1>Hello</h1>
+    );
+}
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Nav from './nav';
+import About from './about';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Nav />
+    <About />
   </React.StrictMode>
 );
 
