d3a818814a3015e057602e89cc7207cb1f899a12
StevenB94 <89290120+StevenB94@users.noreply.github.com>
2024-01-23 20:20:46 -0800

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function Footer(){
     return(
-        <div>Projects</div>
+        <div className="footer">
+            <div>
+
+            </div>
+            <p> &copy; 2024 SB</p>
+        </div>
     )
 }
 
